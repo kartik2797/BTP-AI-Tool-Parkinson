a5946e20d55640ff3fa23cc045be03c5a02c2a51
kartik2797 <43718146+kartik2797@users.noreply.github.com>
2021-04-05 12:55:50 +0530

    added researcher login functionality

--- app/application/researchers/researchers.py
+++ app/application/researchers/researchers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,session,url_for
 from flask import current_app as app
 from flask import render_template,request,redirect,send_file
 from flask_mysqldb import MySQL
@@ -8,6 +8,8 @@ import os
 from .generate import Custom
 from application.models import db
 from application.models import Researcher
+import MySQLdb.cursors
+import re
 
 # Blueprint Config
 researchers_bp = Blueprint(
@@ -56,6 +58,40 @@ def post_selection(name=None):
 
         # Returning to a Template
         return render_template('confirmation.html',fin_selections = fin_selections)
+
+@app.route('/researcherlogin', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
+
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
 
 @researchers_bp.route('/researcher/download',methods=['GET','POST'])
 def download_file():
